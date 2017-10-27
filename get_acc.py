7dc21f26a4c72e1ace22425ec269a4acac5150b8
Naoya Sugita <g1531072@m.sojo-u.ac.jp>
2017-10-27 20:32:40 +0900

    can get acc

---
+++ get_acc.py
@@ -0,0 +1,22 @@
+import smbus
+import time
+
+address = 0x68
+bus = smbus.SMBus(channel)
+
+bus.write_i2c_block_data(address, 0x6B, [0x80])
+time.sleep(0.1)
+
+bus.write_i2c_block_data(address, 0x6B, [0x00])
+time.sleep(0.1)
+
+while True:
+    data = bus.read_i2c_block_data(address, 0x3B ,6)
+    x = (2.0 / float(0x8000)) * (data[0] << 8 | data[1])
+    y = (2.0 / float(0x8000)) * (data[2] << 8 | data[3])
+    z = (2.0 / float(0x8000)) * (data[4] << 8 | data[5])
+    print ("X:%+8.7f" % x)
+    print ("Y:%+8.7f" % y)
+    print ("Z:%+8.7f" % z)
+    time.sleep(1)
+
